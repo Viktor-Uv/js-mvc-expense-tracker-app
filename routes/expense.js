683a82c0378ba2b59bc9281bce74f136fd45b5ee
Viktor Uvarchev <113724227+Viktor-Uv@users.noreply.github.com>
2024-10-16 00:17:03 +0200

    Add routes

--- routes/expense.js
+++ routes/expense.js
@@ -4,15 +4,28 @@ const express = require('express');
 const router = express.Router();
 
 // require the middlewares and callback functions from the controller directory
+const {
+    create,
+    expenseById,
+    read,
+    update,
+    remove,
+    expenseByDate,
+} = require('../controllers');
 
 // Create POST route to create an expense
+router.post("/", create);
 
 // Create GET route to read an expense
+router.get("/:id", expenseById, read);
 
 // Create PUT route to update an expense
+router.put("/:id", expenseById, update);
 
 // Create DELETE route to remove an expense
+router.delete("/:id", remove);
 
 // Create GET route to read a list of expenses
+router.get("/list/:expenseDate", expenseByDate, read);
 
 module.exports = router;
