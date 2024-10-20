4ef4561fd1bec434a8d051eb06bdf06b2344f64a
Viktor Uvarchev <113724227+Viktor-Uv@users.noreply.github.com>
2024-10-20 22:27:41 +0200

    Enable cors

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 require('dotenv').config();
 const expenseRoutes = require('./routes/expense');
+const cors = require('cors');
 
 // Running express server
 const app = express();
 const port = process.env.API_PORT || 8000;
 
 // route middlewares
+app.use(cors());
 app.use('/api', expenseRoutes);
 app.listen(port, () => {
   // eslint-disable-next-line no-console
